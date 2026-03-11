2fc3b19c3031e6a6da9779fc17552f81cdfa54c5
Mo-enen <moenen6@gmail.com>
2026-03-12 00:50:36 +0800

    Daily Update;

--- src/Entry.cs
+++ src/Entry.cs
@@ -23,6 +23,10 @@ Flow.Run(Start, Update, Quit, devName: "Moenen");
 
 static void Start () {
 
+	QWave.SaveWaveFileFromAudioFile(@"D:\Test 0.wav", "");
+	QWave.SaveWaveFileFromAudioFile(@"D:\Test 1.wav", "");
+	QWave.SaveWaveFileFromAudioFile(@"D:\Test 2.wav", "");
+
 }
 
 
--- src/Wave.cs
+++ src/Wave.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using Raylib_cs;
 
 namespace QuickSound;
 
-public class Wave {
+public class QWave {
 
 
 
@@ -14,7 +16,6 @@ public class Wave {
 
 	// Const
 	public const int WAVE_LEN = 256;
-	private static readonly Wave TempWave = new();
 
 	// Api
 	public readonly float[] Data = new float[WAVE_LEN];
@@ -28,7 +29,7 @@ public class Wave {
 	#region --- MSG ---
 
 
-	public Wave () {
+	public QWave () {
 
 	}
 
@@ -52,14 +53,49 @@ public class Wave {
 
 
 	// Audio
-	public static void SaveWaveFileFromAudioFile (string audioFilePath) {
+	public static unsafe void SaveWaveFileFromAudioFile (string audioFilePath, string waveFolderPath) {
 		if (!Util.FileExists(audioFilePath)) return;
 		var wave = Raylib.LoadWave(audioFilePath);
 		if (!Raylib.IsWaveValid(wave)) return;
+		if (wave.SampleCount == 0 || wave.Channels == 0) return;
+		string name = Util.GetNameWithoutExtension(audioFilePath);
+		string waveFilePath = Util.CombinePaths(waveFolderPath, name);
+		using var stream = File.Create(waveFilePath);
+		using var writer = new BinaryWriter(stream);
 		try {
+			Debug.Log(
+				$"name:\"{name}\",",
+				$"ch:{wave.Channels},",
+				$"count:{wave.SampleCount},",
+				$"size:{wave.SampleSize},",
+				$"rate:{wave.SampleRate}"
+			);
+			var data16 = (Half*)wave.Data;
+			var data32 = (float*)wave.Data;
+			var data64 = (double*)wave.Data;
+			int sCount = (int)wave.SampleCount;
+			int sSize = (int)wave.SampleSize;
+			int channelCount = (int)wave.Channels;
+			int singleChLen = sCount / channelCount;
+			for (int i = 0; i < singleChLen; i++) {
+				// Get Sample
+				float sample = 0f;
+				int chOffset = sCount / channelCount;
+				for (int ch = 0; ch < channelCount; ch++) {
+					int index = i + ch * chOffset;
+					float v =
+						sSize == 16 ? (float)data16[index] :
+						sSize == 32 ? data32[index] :
+						sSize == 64 ? (float)data64[index] :
+						0;
+					sample += MathF.Abs(v) / ch;
+				}
+				// Merge Sample
 
 
 
+
+			}
 		} catch (System.Exception ex) { Debug.LogException(ex); }
 		Raylib.UnloadWave(wave);
 	}
